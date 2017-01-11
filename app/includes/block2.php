7339fc3870e38d724a6d9b55f64e1bbcd538b5a7
iwebnal <iwebnal@gmail.com>
2017-01-11 15:00:40 +0300

    block2 1.0

---
+++ app/includes/block2.php
@@ -0,0 +1,12 @@
+<section class="ah-block2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<span class="ah-block2-span">Ландшафтный дизайн<br/>и Архетиктура</span>
+				<img src="img/ah-b2_fr.jpg" class="img-responsive" alt="ah-b2_fr">
+				
+
+			</div>
+		</div>
+	</div>
+</section>
