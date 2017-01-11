50bf1e508dc11a7c028a37c9784f0a7a182d0bc4
iwebnal <iwebnal@gmail.com>
2017-01-11 17:19:37 +0300

    block5

---
+++ app/includes/block4.php
@@ -0,0 +1,18 @@
+<section class="ah-block4">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<p class="ah-block4-text">Что необходимо для создания хорошего сада?<br/>Конечно, хороший проект. Неободим план действий, с помощью которого осуществиться мечта.</p>
+				<span class="ah-block2-span">о мастерской</span>
+				<img src="img/ah-b2-icon3.png" class="img-responsive ah-b2-icon1" alt="ah-b2-icon3">
+				<img src="img/ah-b2_fr2.jpg" class="img-responsive" alt="ah-b2_fr2">
+				<div class="ah-block2-wrap">
+					<p class="ah-block2-text">Специалисты ландшафтной мастерской “Палисандр” уже более пятнадцати лет создают сады. За это время было реализовано порядка 80 проектов и 35 реализованных объектов разной степени сложности. Результат - прекрасные сады наших заказчиков, которые делают этот мир красивее!Ваши сады для нас - обожаемые дети: которые требуют внимания, заботы и любви!</p>
+				</div>
+				<span class="ah-block2-span">наш подход</span>
+				<img src="img/ah-b2-icon4.png" class="img-responsive ah-b2-icon1" alt="ah-b2-icon4">
+				<img src="img/ah-b2_fr3.jpg" class="img-responsive" alt="ah-b2_fr3">
+			</div>
+		</div>
+	</div>
+</section>
---
+++ app/includes/block5.php
@@ -0,0 +1,46 @@
+<section class="ah-block5">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<span class="ah-block2-span">Сад “под ключ”</span>
+				<img src="img/ah-b2-icon5.png" class="img-responsive ah-b2-icon1" alt="ah-b2-icon5">
+				<div class="ah-block5-wrapp">
+					<div class="row">
+						<div class="col-md-5 col-sm-5 col-xs-12">
+							<div class="ah-block5-imgbox1">
+								<img src="img/ah-b5_img1.png" class="img-responsive" alt="ah-b5_img1">
+							</div>
+						</div>
+						<div class="col-md-7 col-sm-7 col-xs-12">
+							<div class="ah-block5-imgbox2">
+								<div class="ah-img1">
+									<img src="img/ah-b5_img2.png" class="img-responsive" alt="ah-b5_img2">
+								</div>
+								<div class="ah-img2">
+									<img src="img/ah-b5_img3.png" class="img-responsive" alt="ah-b5_img3">
+								</div>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+						<div class="col-md-5 col-sm-5 col-xs-12">
+							<div class="ah-block5-imgbox1">
+								<img src="img/ah-b5_img1.png" class="img-responsive" alt="ah-b5_img1">
+							</div>
+						</div>
+						<div class="col-md-7 col-sm-7 col-xs-12">
+							<div class="ah-block5-imgbox2">
+								<div class="ah-img1">
+									<img src="img/ah-b5_img2.png" class="img-responsive" alt="ah-b5_img2">
+								</div>
+								<div class="ah-img2">
+									<img src="img/ah-b5_img3.png" class="img-responsive" alt="ah-b5_img3">
+								</div>
+							</div>
+						</div>
+
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
