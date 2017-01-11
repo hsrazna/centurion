218988516cf9883ef8e5b4c9ac8457861c6f5c88
hsrazna <anzar.terek@gmail.com>
2017-01-11 20:02:10 +0300

    footer and etc.

---
+++ app/includes/block8.php
@@ -0,0 +1,11 @@
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="az-img2" style="background: url(img/trees.jpg) no-repeat top center; background-size: cover">
+					<a href="#">Оставить заявку</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
