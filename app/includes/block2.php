bd1b138d86e10c44ace1b26671f749abb00b487f
iwebnal <iwebnal@gmail.com>
2017-01-11 15:28:53 +0300

    block2 fonts

--- app/includes/block2.php
+++ app/includes/block2.php
@@ -3,8 +3,9 @@
 		<div class="row">
 			<div class="col-md-12 col-sm-12 col-xs-12">
 				<span class="ah-block2-span">Ландшафтный дизайн<br/>и Архетиктура</span>
+				<img src="img/ah-b2-icon.png" class="img-responsive ah-b2-icon1" alt="ah-b2-icon">
 				<img src="img/ah-b2_fr.jpg" class="img-responsive" alt="ah-b2_fr">
-				
+
 
 			</div>
 		</div>
